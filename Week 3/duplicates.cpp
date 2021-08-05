db35f73ebec873e655cdf6aab4eae5bfe87379f7
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-08-05 12:07:44 +0530

    Sorting

--- Week 3/duplicates.cpp
+++ Week 3/duplicates.cpp
@@ -7,60 +7,60 @@ Question 3: Implementation of Sorting Algorithm having time complexity O(nlogn).
 #include<iostream>
 using namespace std;
 
-void merge(int *array, int l, int m, int r) 
+void merge(int *arr, int l, int m, int r) 
 {
     int i, j, k, nl, nr;
     nl = m-l+1; nr = r-m;
     int larr[10], rarr[10];
    
     for(i = 0; i<nl; i++)
-        larr[i] = array[l+i];
+        larr[i] = arr[l+i];
     for(j = 0; j<nr; j++)
-        rarr[j] = array[m+1+j];
+        rarr[j] = arr[m+1+j];
         i = 0; j = 0; k = l;
   
     while(i < nl && j<nr)
     {
         if(larr[i] <= rarr[j]) 
         {
-            array[k] = larr[i];
+            arr[k] = larr[i];
             i++;
         }
         else
         {
-            array[k] = rarr[j];
+            arr[k] = rarr[j];
             j++;
         }
       k++;
    }
    while(i<nl) 
    {       
-        array[k] = larr[i];
+        arr[k] = larr[i];
         i++; k++;
    }
    while(j<nr) 
    {     
-        array[k] = rarr[j];
+        arr[k] = rarr[j];
         j++; k++;
    }
 }
 
-void merge_sort(int *array, int l, int r) {
+void merge_sort(int *arr, int l, int r) {
    int m;
    if(l < r) {
       int m = l+(r-l)/2;
       
-      merge_sort(array, l, m);
-      merge_sort(array, m+1, r);
-      merge(array, l, m, r);
+      merge_sort(arr, l, m);
+      merge_sort(arr, m+1, r);
+      merge(arr, l, m, r);
    }
 }
 
-void display(int *array, int size) 
+void display(int *arr, int size) 
 {
     int flag=0;
     for(int i = 0; i<size; i++)
-    if(array[i]==array[i+1])
+    if(arr[i]==arr[i+1])
     {
         flag=1;
     }
