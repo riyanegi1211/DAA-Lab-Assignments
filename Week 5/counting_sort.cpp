db5c5785c105730978833b0e49702f140e84ec64
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-09-23 22:39:58 +0530

    Duplicate Alphabets

--- Week 5/counting_sort.cpp
+++ Week 5/counting_sort.cpp
@@ -1,3 +1,36 @@
 /* WEEK 5
-Question 1: To find duplicate numbers by implementing Counting Sort having time complexity O(n)
+Question 1: To find frequency of duplicate alphabets by implementing Counting Sort having time complexity O(n)
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i,t;
+    cin>>t; 
+    while(t--){
+        cin>>n; 
+        char arr[n]; 
+        
+        for(i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int freq[26]={0};
+        for(int i=0;i<n;i++){
+            freq[arr[i]-'a']++;
+        }
+        int max=0,in;
+        for(int i=0;i<26;i++){
+            if(freq[i]>max){
+                max=freq[i];
+                in=i;
+            }
+        }
+        if(max>1)
+            cout<<char('a'+in)<<" : "<<max<<endl;
+        else
+            cout<<"No Duplicates Present"<<endl;
+    }
+    return 0;
+}
