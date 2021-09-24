4453f05e4bcd10b610f17a8b4a9b0d548db8e186
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-09-24 21:26:41 +0530

    Searching Algorithm

--- Week 1/linear_search.cpp
+++ Week 1/linear_search.cpp
@@ -1,40 +1,49 @@
-#include<iostream>
+/*WEEK 0
+Question 1: Implementation of Linear Search Algorithm
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/ 
+
+#include <iostream>
 
 using namespace std;
 
-void linear_search(int arr[], int N,int key){
-int flag=0,count=0,i;
-    for(i=0;i<N;i++){
-        if(arr[i]==key){
+void linear_search(int arr[],int N,int key)
+{
+    int i, flag=0,count=0;
+    for(i=0;i<N;i++)
+    {
+        if(arr[i]==key)
+        {   
             flag=1;
             count++;
             break;
         }
         else
-            count++;
+        count++;
     }
-
     if(flag==1)
-        cout<<"Present "<<count<<endl; 
+    cout<<"Present "<<count<<endl;
     else
-        cout<<"Not Present "<<count<<endl;
-    
+    cout<<"Not Present "<<count<<endl;
 }
+
 int main()
 {
-    int arr[100],t,j,i,N,key;
-    cin>>t;                     // Enter the No. of test cases
-
-    for(j=0;j<t;j++)
+  int arr[100],N,key,i,j,t;
+  cin>>t;  
+                  
+  for(j=0;j<t;j++)
+    {
+        cin>>N;            
+        for(i=0;i<N;i++)
         {
-            cin>>N;     // Enter the size of array
-            for(i=0;i<N;i++)
-            {
-                cin>>arr[i];    // Enter the lements of array
-            }
-            
-            cin>>key;           // Enter the value of key 
-            linear_search(arr,N,key);
+            cin>>arr[i];    
         }
-    return 0;
+
+        cin>>key;            
+    linear_search(arr,N,key);
+    }
+  return 0;
 }
