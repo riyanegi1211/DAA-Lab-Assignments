d7744988d1c0e2d8161a6e9eb80bbc66e01d0027
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-09-25 10:13:33 +0530

    Duplicates

--- Week 2/duplicate.cpp
+++ Week 2/duplicate.cpp
@@ -1,3 +1,13 @@
+/*WEEK 2
+Question 1. Given a sorted array of positive integers containing few duplicate elements,
+design an algorithm and implement it using a program to find whether the given
+key element is present in the array or not. If present, then also find the number
+of copies of given key. 
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
 #include<iostream>
 using namespace std;
 
